3a8d07269d5172b67a1ea38d0f83bf023a962501
Marco Rosato <145607554+capflint92@users.noreply.github.com>
2024-03-08 21:56:42 +0100

    Aggiunta file

---
+++ index.js
@@ -0,0 +1,9 @@
+/* ----- TYPING EFFECT ----- */
+let typingEffect = new Typed(".typedText", {
+    strings: ["Historian", "Developer", "Dreamer", "Videogamer"],
+    loop: true,
+    typeSpeed: 100,  // Adjust typing speed (characters per second)
+    backSpeed: 80,   // Adjust backspace speed (characters per second)
+    backDelay: 1000   // Delay in milliseconds before backspacing
+  });
+  
